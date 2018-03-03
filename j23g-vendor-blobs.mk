d8c37e3b8a550490e9068bb27fc571fbec791132
almamun512 <iamalmamun11@gmail.com>
2018-03-04 00:36:25 +0600

    misc.
    Signed-off-by: almamun512 <iamalmamun11@gmail.com>

--- j23g-vendor-blobs.mk
+++ j23g-vendor-blobs.mk
@@ -17,8 +17,8 @@
 PROPRIETARY_FILES := \
     lib/libisp.so \
     lib/libface_finder.so \
-    lib/hw/camera.sc8830.so \
     lib/hw/sensors.sc8830.so \
+    lib/hw/camera.sc8830.so \
     usr/keylayout/Generic.kl
 PRODUCT_COPY_FILES += \
     $(foreach f,$(PROPRIETARY_FILES),vendor/samsung/j23g/proprietary/$(f):system/$(f))
